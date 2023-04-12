1ea1f510414ee85ca3d92c368b0334dadfb8c692
Counsell1111 <jacob.counsell@netdocuments.com>
2023-04-12 12:34:07 -0400

    xx

--- stories/Buttons.jsx
+++ stories/Buttons.jsx
@@ -1,3 +1,4 @@
+import { makeStyles, Button } from "@fluentui/react-components";
 import React from 'react';
 import PropTypes from 'prop-types';
 import './button.css';
@@ -8,14 +9,7 @@ import './button.css';
 export const Button = ({ primary, backgroundColor, size, label, ...props }) => {
   const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
   return (
-    <button
-      type="button"
-      className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
-      style={backgroundColor && { backgroundColor }}
-      {...props}
-    >
-      {label}
-    </button>
+    <Button shape="circular">Circular</Button>
   );
 };
 
