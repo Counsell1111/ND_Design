5feb45dc68f2f660a3f953d53a9eae1bbb45b2b5
Counsell1111 <jacob.counsell@netdocuments.com>
2023-04-12 16:29:13 -0400

    xx

--- stories/Inputs.jsx
+++ stories/Inputs.jsx
@@ -1,51 +1,22 @@
-import { makeStyles, Button } from "@fluentui/react-components";
+import {  
+    shorthands,
+    useId,
+    Input,
+    Label,
+    InputProps, } from "@fluentui/react-components";
 import React from 'react';
-import PropTypes from 'prop-types';
-import './button.css';
-
-
-const useStyles = makeStyles({
-  innerWrapper: {
-    columnGap: "15px",
-    display: "flex",
-  },
-  outerWrapper: {
-    display: "flex",
-    flexDirection: "column",
-    rowGap: "15px",
-  },
-  ndbutton: {
-    background: "#000",
-  },  
-});
 
 /**
  * Primary UI component for user interaction
  */
-export const Buttons = ({ size, typer, label, ...props }) => {
-
-  const styles = useStyles();
+export const Inputs = () => {
+    const inputId = useId("input");
   return (
-    <Button 
-    appearance={typer}
-    size={size}
-    {...props}
-    >{label}</Button>
+    <div>
+    <Label htmlFor={inputId} size={props.size} disabled={props.disabled}>
+      Sample input
+    </Label>
+    <Input id={inputId} {...props} />
+  </div>
    );
-};
-
-Button.propTypes = {
-  primary: PropTypes.bool,
-  backgroundColor: PropTypes.string,
-  size: PropTypes.oneOf(['small', 'medium', 'large']),
-  typer: PropTypes.oneOf(['primary', 'outline', 'subtle', 'transparent']),
-  label: PropTypes.string.isRequired,
-  onClick: PropTypes.func,
-};
-
-Button.defaultProps = {
-  backgroundColor: null,
-  primary: false,
-  size: 'medium',
-  onClick: undefined,
 };
--- stories/Inputs.stories.js
+++ stories/Inputs.stories.js
@@ -3,41 +3,13 @@ import { Buttons } from './Buttons';
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
 export default {
   title: 'Components/Inputs',
-  component: Buttons,
+  component: Inputs,
   tags: ['autodocs'],
   argTypes: {
-    backgroundColor: { control: 'color' },
-    appearance: { control: 'select', options: ['primary', 'outline', 'subtle', 'transparent'] },
-    size: { control: 'select', options: ['small', 'medium', 'large'] }               
+            
   },
 };
 
-// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
 export const Primary = {
-  args: {
-    label: 'Button',
-    size:'medium',
-    appearance: 'primary',
-  },
-};
 
-export const Secondary = {
-  args: {
-    size:'medium',
-    label: 'Button',
-  },
-};
-
-export const Large = {
-  args: {
-    size: 'large',
-    label: 'Button',
-  },
-};
-
-export const Small = {
-  args: {
-    size: 'small',
-    label: 'Button',
-  },
 };
