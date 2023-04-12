f224bfd017311432773cf874bcd90c9dc7f2c4f1
Counsell1111 <jacob.counsell@netdocuments.com>
2023-04-12 16:33:06 -0400

    xx

--- stories/Inputs.stories.js
+++ stories/Inputs.stories.js
@@ -1,6 +1,5 @@
-import { Buttons } from './Buttons';
+import { Inputs } from './Inputs';
 
-// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
 export default {
   title: 'Components/Inputs',
   component: Inputs,
