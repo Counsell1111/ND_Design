fe4d151d6edf90bdfdf852e094a5c767afeb4f63
Counsell1111 <jacob.counsell@netdocuments.com>
2023-04-14 15:15:29 -0400

    xx

--- stories/Headers.stories.js
+++ stories/Headers.stories.js
@@ -3,7 +3,7 @@ import { Header } from './Headers';
 
 export default {
   title: 'Components/Headers',
-  component: Headers,
+  component: Header,
   tags: ['autodocs'],
   argTypes: {             
   },
