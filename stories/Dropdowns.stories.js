ad6cfb899081cd3b6a668b28952aeb39a1f0873f
Counsell1111 <jacob.counsell@netdocuments.com>
2023-04-17 18:36:57 -0400

    xx

---
+++ stories/Dropdowns.stories.js
@@ -0,0 +1,35 @@
+import { Dropdowns } from './Dropdowns';
+
+export default {
+  title: 'Components/Dropdowns',
+  component: Dropdowns,
+  tags: ['autodocs'],
+  argTypes: {
+     
+  },
+};
+
+export const Primary = {
+    args: {
+
+      },
+};
+
+export const Secondary = {
+    args: {
+
+    },
+  };
+  
+  export const Large = {
+    args: {
+
+    },
+  };
+  
+  export const Small = {
+    args: {
+
+    },
+  };
+  
---
+++ stories/Dropdowns.tsx
@@ -0,0 +1,65 @@
+import {  
+    Dropdown,
+    makeStyles,
+    Option,
+    shorthands,
+    useId,
+    DropdownProps, } from "@fluentui/react-components";
+import React from 'react';
+import PropTypes from 'prop-types';
+  
+  const useStyles = makeStyles({
+    root: {
+      // Stack the label above the field with a gap
+      display: "grid",
+      gridTemplateRows: "repeat(1fr)",
+      justifyItems: "start",
+      ...shorthands.gap("2px"),
+      maxWidth: "400px",
+    },
+  });
+
+export const Dropdowns = ({ size, label, typer, ...props }) => {
+    const dropdownId = useId("dropdown-default");
+    const options = [
+      "Cat",
+      "Caterpillar",
+      "Corgi",
+      "Chupacabra",
+      "Dog",
+      "Ferret",
+      "Fish",
+      "Fox",
+      "Hamster",
+      "Snake",
+    ];
+    const styles = useStyles();
+    return (
+      <div className={styles.root}>
+        <label id={dropdownId}>Best pet</label>
+        <Dropdown
+          aria-labelledby={dropdownId}
+          placeholder="Select an animal"
+          {...props}
+        >
+          {options.map((option) => (
+            <Option key={option} disabled={option === "Ferret"}>
+              {option}
+            </Option>
+          ))}
+        </Dropdown>
+      </div>
+    );
+};
+
+
+Dropdowns.propTypes = {
+
+  };
+  
+  Dropdowns.defaultProps = {
+
+  };
+
+
+ 
