0e4d0d0a88604fd86af280ebdaa402f42e4326a6
Counsell1111 <jacob.counsell@netdocuments.com>
2023-04-20 17:16:25 -0400

    xx

--- stories/Fields.jsx
+++ stories/Fields.jsx
@@ -1,24 +1,13 @@
 
 import {
-    makeStyles,
-    shorthands,
+
     Field,
     Input,
   } from "@fluentui/react-components";
   import React from 'react';
   import PropTypes from 'prop-types';
     
-  const useStyles = makeStyles({
-    root: {
-       display: "flex",
-       flexDirection: "column",
-       ...shorthands.gap("2px"),
-       maxWidth: "400px",
-    },
-  });
-  
   export const Fields = ({ }) => {
-      const styles = useStyles();
       return (
             <Field>
     <Input />
