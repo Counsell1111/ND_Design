937376608ef6f7f670f3ccd0be41c2c1e9409e4c
Counsell1111 <jacob.counsell@netdocuments.com>
2023-04-21 11:10:32 -0400

    xx

--- stories/Tablist.jsx
+++ stories/Tablist.jsx
@@ -1,42 +1,41 @@
 import {  
-    shorthands,
     makeStyles,
-    useId,
-    Input,
-    Label,
-    InputProps, } from "@fluentui/react-components";
+    shorthands,
+    Tab,
+    TabList, } from "@fluentui/react-components";
 import React from 'react';
 import PropTypes from 'prop-types';
 
 const useStyles = makeStyles({
     root: {
-       display: "flex",
-       flexDirection: "column",
-       ...shorthands.gap("2px"),
-       maxWidth: "400px",
+        alignItems: "flex-start",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "flex-start",
+        ...shorthands.padding("50px", "20px"),
+        rowGap: "20px",
     },
   });
 
-export const Inputs = ({ size, label, typer, ...props }) => {
-    const inputId = useId("input");
+export const Inputs = ({ }) => {
     const styles = useStyles();
   return (
     <div className={styles.root}>
-    <Label size={size} htmlFor={inputId}>
-    {label}
-    </Label>
-    <Input size={size} placeholder={label} id={inputId} appearance={typer}/>
-  </div>
+      <TabList defaultSelectedValue="tab2">
+        <Tab value="tab1">First Tab</Tab>
+        <Tab value="tab2">Second Tab</Tab>
+        <Tab value="tab3">Third Tab</Tab>
+        <Tab value="tab4">Fourth Tab</Tab>
+      </TabList>
+    </div>
    );
 };
 
 
 Input.propTypes = {
-    size: PropTypes.oneOf(['small', 'medium', 'large']),
-    typer: PropTypes.oneOf(['outline','underline','filled-darker','filled-lighter','filled-darker-shadow','filled-lighter-shadow']),
+
   };
   
   Input.defaultProps = {
-    size: 'medium',
-    typer: 'outline',
+
   };
--- stories/Tablist.stories.js
+++ stories/Tablist.stories.js
@@ -5,36 +5,31 @@ export default {
   component: Tablist,
   tags: ['autodocs'],
   argTypes: {
-    size: { control: 'select', options: ['small', 'medium', 'large'] }, 
-    typer: { control: 'select', options: ['outline','underline','filled-darker','filled-lighter','filled-darker-shadow','filled-lighter-shadow'] }       
+     
   },
 };
 
 export const Primary = {
     args: {
-        label: 'Placeholder',
-        size:'medium',
+  
       },
 };
 
 export const Secondary = {
     args: {
-      label: 'Placeholder',
-      size:'medium',
+
     },
   };
   
   export const Large = {
     args: {
-      label: 'Placeholder',
-      size: 'large',
+ 
     },
   };
   
   export const Small = {
     args: {
-      label: 'Placeholder',
-      size: 'small',
+
     },
   };
   
