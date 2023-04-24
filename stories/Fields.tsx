5f9703193242530e83f4112cad196f76563cd67d
Counsell1111 <jacob.counsell@netdocuments.com>
2023-04-24 10:10:05 -0400

    xx

---
+++ stories/Fields.tsx
@@ -0,0 +1,47 @@
+
+import {
+    shorthands,
+    Field,
+    Input,
+    makeStyles,
+  } from "@fluentui/react-components";
+  import React from 'react';
+  import PropTypes from 'prop-types';
+    
+  const useStyles = makeStyles({
+    root: {
+       display: "flex",
+       flexDirection: "column",
+       ...shorthands.gap("2px"),
+       maxWidth: "400px",
+    },
+  });
+  
+  export const fields = ({ size, label, validate_state, validate_text, ...props }) => {
+      const styles = useStyles();
+      return (
+          <div className={styles.root}>
+            <Field
+     placeholder={label}
+     size={size}
+     validationState={validate_state}
+     validationMessage={validate_text}
+    {...props}
+  >
+    <Input />
+  </Field>
+        </div>
+      );
+  };
+  
+  
+ fields.propTypes = {
+    size: PropTypes.oneOf(['small', 'medium', 'large']),
+    validate_state: PropTypes.oneOf(['none', 'success', 'warning', 'error']),
+  };
+  
+  fields.defaultProps = {
+    size: 'medium',
+    validate_state: 'success',
+  };
+  
