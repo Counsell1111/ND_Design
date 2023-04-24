1762e1d677716750ab8b54d955735a76f13a9bbd
Counsell1111 <jacob.counsell@netdocuments.com>
2023-04-24 17:28:53 -0400

    xx

---
+++ stories/MenuButtons.jsx
@@ -0,0 +1,44 @@
+import {  
+    Menu,
+    MenuButton,
+    MenuItem,
+    MenuList,
+    MenuPopover,
+    MenuTrigger, } from "@fluentui/react-components";
+import React from 'react';
+import PropTypes from 'prop-types';
+import './global.css';
+
+export const menubutton = ({ disabled, size, appearance, label }) => {
+  return (
+   <Menu>
+   <MenuTrigger disableButtonEnhancement>
+     <MenuButton
+       appearance={appearance}
+       size={size}
+       {...{disabled}}>{label}</MenuButton>
+   </MenuTrigger>
+
+   <MenuPopover>
+     <MenuList>
+       <MenuItem>Item a</MenuItem>
+       <MenuItem>Item b</MenuItem>
+       <MenuItem>Item c</MenuItem>
+       <MenuItem>Item d</MenuItem>
+     </MenuList>
+   </MenuPopover>
+ </Menu>
+   );
+};
+
+menubutton.propTypes = {
+  size: PropTypes.oneOf(['small', 'medium', 'large']),
+  appearance: PropTypes.oneOf(['primary', 'outline', 'subtle', 'transparent']),
+  disabled: PropTypes.oneOf([ 'disabled', '']),
+};
+
+menubutton.defaultProps = {
+  size: 'medium',
+  appearance: 'primary',
+  disabled: '',
+};
---
+++ stories/MenuButtons.stories.js
@@ -0,0 +1,54 @@
+import { menubutton } from './MenuButtons';
+
+export default {
+  title: 'Patterns/Buttons/MenuButton',
+  component: menubutton,
+  tags: ['autodocs'],
+  argTypes: {
+    appearance: { control: 'inline-radio', options: ['primary', 'outline', 'subtle', 'transparent'] },
+    size: { control: 'inline-radio', options: ['small', 'medium', 'large'] },    
+    disabled: { control: 'boolean', options: [ 'disabled', ''] }         
+  },
+};
+
+export const Primary = {
+  args: {
+    label: 'Button',
+    size:'medium',
+    appearance: 'primary',
+    disabled: '',
+  },
+};
+
+
+export const Small = {
+  args: {
+    size: 'small',
+    label: 'Small Button',
+    disabled: '',
+  },
+};
+
+export const Medium = {
+  args: {
+    size:'medium',
+    label: 'Medium Button',
+    disabled: '',
+  },
+};
+
+export const Large = {
+  args: {
+    size: 'large',
+    label: 'Large Button',
+    disabled: '',
+  },
+};
+
+export const Disabled = {
+  args: {
+    size: 'large',
+    label: 'Disabled Button',
+    disabled: 'disabled',
+  },
+};
