e5fe4216c401163d80cb9215a6d552c89d3a94d6
Counsell1111 <jacob.counsell@netdocuments.com>
2023-04-24 17:45:58 -0400

    xx

---
+++ stories/SplitButtons.tsx
@@ -0,0 +1,46 @@
+import {  
+    Menu,
+    MenuItem,
+    MenuList,
+    MenuPopover,
+    MenuTrigger,
+    SplitButton,
+    MenuButtonProps, } from "@fluentui/react-components";
+import React from 'react';
+import PropTypes from 'prop-types';
+import './global.css';
+
+export const splitbutton = ({ disabled, size, appearance, label }) => {
+  return (
+<Menu positioning="below-end">
+<MenuTrigger disableButtonEnhancement>
+  {(triggerProps: MenuButtonProps) => (
+    <SplitButton  appearance={appearance}
+    size={size}
+    {...{disabled}}>{label}</SplitButton>
+  )}
+</MenuTrigger>
+
+<MenuPopover>
+  <MenuList>
+       <MenuItem>Option One</MenuItem>
+       <MenuItem>Option Two</MenuItem>
+       <MenuItem>Option Three</MenuItem>
+       <MenuItem>Option Four</MenuItem>
+  </MenuList>
+</MenuPopover>
+</Menu>
+   );
+};
+
+splitbutton.propTypes = {
+  size: PropTypes.oneOf(['small', 'medium', 'large']),
+  appearance: PropTypes.oneOf(['', 'primary', 'outline', 'subtle', 'transparent']),
+  disabled: PropTypes.oneOf([ 'disabled', '']),
+};
+
+splitbutton.defaultProps = {
+  size: 'medium',
+  appearance: '',
+  disabled: '',
+};
