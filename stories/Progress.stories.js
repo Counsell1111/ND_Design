112c83e4238eee3cf2aa2c4f5dc3e356e5fcae54
Counsell1111 <jacob.counsell@netdocuments.com>
2023-04-24 12:42:35 -0400

    Update Progress.stories.js

--- stories/Progress.stories.js
+++ stories/Progress.stories.js
@@ -12,35 +12,48 @@ export default {
 
 export const Primary = {
     args: {
+        size:'medium',
+        validate_text: 'This is a status message',
+        validate_state: 'none',
       },
 };
 
 export const Medium = {
     args: {
-
+        size:'medium',
+        validate_text: 'This is a status message',
+        validate_state: 'none',
     },
   };
   
   export const Large = {
     args: {
-
+        size:'medium',
+        validate_text: 'This is a status message',
+        validate_state: 'none',
     },
   };
 
   export const Success = {
     args: {
-
+        size:'medium',
+        validate_text: 'This is a status message',
+        validate_state: 'none',
     },
   };
 
   export const Warning = {
     args: {
-
+        size:'medium',
+        validate_text: 'This is a status message',
+        validate_state: 'none',
     },
   };
 
   export const Error = {
     args: {
- 
+        size:'medium',
+        validate_text: 'This is a status message',
+        validate_state: 'none',
     },
   };
