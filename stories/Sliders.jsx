2d8bb7137261c41bd93737e307a4a404796f35b0
Counsell1111 <jacob.counsell@netdocuments.com>
2023-04-25 12:18:37 -0400

    xx

--- stories/Sliders.jsx
+++ stories/Sliders.jsx
@@ -2,17 +2,29 @@ import { makeStyles, useId, Label, Slider  } from "@fluentui/react-components";
 import React from 'react';
 import PropTypes from 'prop-types';
 
+
+const useStyles = makeStyles({
+    root: {
+       display: "flex",
+       flexDirection: "column",
+       ...shorthands.gap("2px"),
+       maxWidth: "400px",
+    },
+  });
+
+
 export const slider = ({size, label, disabled, vertical }) => {
   const id = useId();
+  const styles = useStyles();
   return (
-    <>
+    <div className={styles.root}>
       <Label htmlFor={id}>{label}</Label>
       <Slider       
       size={size}
       {...{disabled}}
       {...{vertical}}
       defaultValue={20} id={id} />
-    </>
+    </div>
    );
 };
 
