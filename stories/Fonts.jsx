0fefeb6b45a05ca6b9705b7db4b37c9ef2d35bb8
Counsell1111 <jacob.counsell@netdocuments.com>
2023-04-27 09:17:38 -0400

    xx

---
+++ stories/Fonts.jsx
@@ -0,0 +1,21 @@
+import { text } from "@fluentui/react-components";
+import React from 'react';
+import PropTypes from 'prop-types';
+  
+  
+  export const text = ({}) => {
+      return (
+        <Text>This is an example of the Text component's usage.</Text>
+      );
+  };
+  
+  
+text.propTypes = {
+
+  };
+  
+text.defaultProps = {
+
+  };
+  
+  
---
+++ stories/Fonts.stories.js
@@ -0,0 +1,15 @@
+import { text } from './Fonts';
+
+export default {
+  title: 'Foundations/Typography/Text',
+  component: text,
+  tags: ['autodocs'],
+  argTypes: {  
+  },
+};
+
+export const Primary = {
+    args: {
+
+    },
+  };
