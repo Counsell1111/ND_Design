839d998ff95f33111b1259f227157021ae01b78d
Counsell1111 <jacob.counsell@netdocuments.com>
2023-04-27 10:08:38 -0400

    xx

--- stories/Fonts.jsx
+++ stories/Fonts.jsx
@@ -5,17 +5,44 @@ import PropTypes from 'prop-types';
   
   export const text = ({}) => {
       return (
-        <Text>This is an example of the Text component's usage.</Text>
+        <Text 
+        size={size} 
+        weight={weight} 
+        align={align} 
+        wrap={wrap} 
+        font={font}
+        {...{underline}}
+        {...{strikethrough}}
+        {...{truncate}}
+        {...{italic}}>
+            {label}
+            </Text>
       );
   };
   
   
 text.propTypes = {
-
+    size: PropTypes.oneOf(['100','200','300','400','500','600','700','800','900','1000']),
+    weight: PropTypes.oneOf(['medium','regular','semibold','bold']),
+    align: PropTypes.oneOf(['center','start','justify','end']),
+    font: PropTypes.oneOf(['base','numeric','monospace']),
+    wrap: PropTypes.oneOf(['true', 'false']),
+    underline: PropTypes.oneOf([ 'underline', '']),
+    strikethrough: PropTypes.oneOf(['strikethrough', '']),
+    truncate: PropTypes.oneOf(['truncate', '']),
+    italic: PropTypes.oneOf(['italic', '']),
   };
   
 text.defaultProps = {
-
+    size: '100',
+    weight: 'regular',
+    align: 'center',
+    font: 'base',
+    wrap: 'false',
+    italic:'',
+    underline: '',
+    strikethrough: '',
+    truncate:'',
   };
   
   
--- stories/Fonts.stories.js
+++ stories/Fonts.stories.js
@@ -5,11 +5,28 @@ export default {
   component: text,
   tags: ['autodocs'],
   argTypes: {  
+    size: { control: 'inline-radio', options: ['100','200','300','400','500','600','700','800','900','1000'] }, 
+    weight: { control: 'inline-radio', options: ['medium','regular','semibold','bold'] },   
+    font: { control: 'inline-radio', options: ['base','numeric','monospace'] },
+    align: { control: 'inline-radio', options: ['center','start','justify','end'] },     
+    underline: { control: 'boolean', options: [ 'underline', ''] },  
+    stikethrough: { control: 'boolean', options: [ 'stikethrough', ''] },  
+    italic: { control: 'boolean', options: [ 'italic', ''] }, 
+    wrap: { control: 'boolean', options: [ 'true', 'false'] },  
+    truncate: { control: 'boolean', options: [ 'truncate', ''] }, 
   },
 };
 
 export const Primary = {
     args: {
-
+        size: '100',
+        weight: 'regular',
+        align: 'center',
+        font: 'base',
+        wrap: 'false',
+        italic:'',
+        underline: '',
+        strikethrough: '',
+        truncate:'',
     },
   };
