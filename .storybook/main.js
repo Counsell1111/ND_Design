d4fd6e1583885f5771c99e3e166233881381a151
Counsell1111 <jacob.counsell@netdocuments.com>
2023-04-28 10:34:49 -0400

    xx

--- .storybook/main.js
+++ .storybook/main.js
@@ -5,6 +5,7 @@ const config = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
+    "@storybook/addon-storysource",
   ],
   framework: {
     name: "@storybook/react-webpack5",
