d8a06818c468fa8add00580b5d909d66150882c7
Counsell1111 <jacob.counsell@netdocuments.com>
2023-05-04 18:00:37 -0400

    xx

--- stories/Form.jsx
+++ stories/Form.jsx
@@ -60,7 +60,7 @@ import './global.css';
   </div>
 
   <div className="addin_foot">
-  <Button styles={{width:"100%",}}>{addin_button}</Button>
+  <Button appearance="primary" styles={{width:"100%",}}>{addin_button}</Button>
 </div>
   </div>
   </div>
