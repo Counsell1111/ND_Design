9acfd4a4e00a26fd59bf2bafb361531ed7dc674b
Counsell1111 <jacob.counsell@netdocuments.com>
2023-05-04 11:15:15 -0400

    xx

--- stories/Labels.stories.js
+++ stories/Labels.stories.js
@@ -6,16 +6,78 @@ export default {
   tags: ['autodocs'],
   argTypes: {
     weight: { control: 'inline-radio', options: ['regular', 'semibold'] },
-    size: { control: 'inline-radio', options: ['small', 'medium', 'large'] } 
+    size: { control: 'inline-radio', options: ['small', 'medium', 'large'] },
+    disabled: { control: 'boolean', options: [ 'disabled', ''] },   
+    required: { control: 'boolean', options: [ 'required', ''] }  
   },
 };
 
 export const Primary = {
     args: {
-        label: 'Default Label',
+        label: 'Primary Label',
         size:'medium',
         weight: 'regular',
         disabled: '',
         required: '',
+      },
+};
+
+export const Small = {
+    args: {
+        label: 'Small Label',
+        size:'small',
+        weight: 'regular',
+        disabled: '',
+        required: '',
+      },
+};
+
+export const Medium = {
+    args: {
+        label: 'Medium Label',
+        size:'medium',
+        weight: 'regular',
+        disabled: '',
+        required: '',
+      },
+};
+
+export const Large = {
+    args: {
+        label: 'Large Label',
+        size:'large',
+        weight: 'regular',
+        disabled: '',
+        required: '',
+      },
+};
+
+export const Bold = {
+    args: {
+        label: 'Semibold Label',
+        size:'medium',
+        weight: 'semibold',
+        disabled: '',
+        required: '',
+      },
+};
+
+export const Disabled = {
+    args: {
+        label: 'Disabled Label',
+        size:'medium',
+        weight: 'regular',
+        disabled: 'disabled',
+        required: '',
+      },
+};
+
+export const Required = {
+    args: {
+        label: 'Required Label',
+        size:'medium',
+        weight: 'regular',
+        disabled: '',
+        required: 'required',
       },
 };
