2af20bb9b8ae18bdbe0dc427f44d596c561666af
Counsell1111 <jacob.counsell@netdocuments.com>
2023-05-04 15:01:45 -0400

    xx

---
+++ stories/Form.jsx
@@ -0,0 +1,47 @@
+import {  
+ } from "@fluentui/react-components";
+  import { ChevronDown24Regular, List28Filled, Search28Filled } from "@fluentui/react-icons";
+  import React from 'react';
+  import './global.css';
+  
+  
+  const useStyles = makeStyles({
+    root: {
+      width: "100%",
+      top: "0px",
+      left: "0px",
+      right: "0px",
+      height:"32px",
+      display: "flex",
+      flexDirection: "column",
+      ...shorthands.gap("2px"),
+      minWidth: "750px",
+   },
+   innerWrapper: {
+    columnGap: "15px",
+    display: "flex",
+  },
+  outerWrapper: {
+    display: "flex",
+    flexDirection: "column",
+    rowGap: "15px",
+  },
+  });
+  
+  export const Add_Form = () => {
+    const styles = useStyles();
+    return (
+      <div className={styles.root}>
+  <div className="addin_form">
+
+  </div>
+  </div>
+     );
+  };
+  
+  Add_Form.propTypes = {
+  };
+  
+  Add_Form.defaultProps = {
+  };
+  
---
+++ stories/Form.stories.js
@@ -0,0 +1,15 @@
+import { Add_Form } from './Form';
+
+
+export default {
+  title: 'Patterns/Form',
+  component: Add_Form,
+  tags: ['autodocs'],
+  argTypes: {             
+  },
+};
+
+export const Primary = {
+  args: {
+  },
+};
