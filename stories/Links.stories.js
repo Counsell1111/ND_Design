4bc03e285dc11938bbe22c11be6ac89723b5aefe
Counsell1111 <jacob.counsell@netdocuments.com>
2023-05-04 14:49:13 -0400

    Update Links.stories.js

--- stories/Links.stories.js
+++ stories/Links.stories.js
@@ -12,10 +12,25 @@ export default {
 
 export const Primary = {
     args: {
-        label: 'Default Button',
+        label: 'Default Link',
         appearance: 'default',
         disabled: '',
       },
 };
 
+export const Subtle = {
+    args: {
+        label: 'Default Link',
+        appearance: 'subtle',
+        disabled: '',
+      },
+};
+
+export const Disabled = {
+    args: {
+        label: 'Default Link',
+        appearance: 'default',
+        disabled: 'disabled',
+      },
+};
   
