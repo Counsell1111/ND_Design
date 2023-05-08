2df25fca0750f86d1f8eccc6f9515e9ead7993e4
Counsell1111 <jacob.counsell@netdocuments.com>
2023-05-08 12:39:30 -0400

    xx

--- stories/Form.tsx
+++ stories/Form.tsx
@@ -102,7 +102,7 @@ import './global.css';
     <Textarea rows={5} placeholder="Access the document “Draft Doc” v2 of 3 by clicking the link embedded in this email." />
     <div className="text_foot">
     <Button size="large" appearance="subtle" icon={<Link28Filled />} />
-    <div className="doc_foot"><DocumentBulletList24Regular /><Body1>Draft Doc.doc v2 of 3</Body1></div>
+    <div className="doc_foot"><Body1>Draft Doc.doc v2 of 3</Body1></div>
       <div className="doc_link"><Link>
       Add Receipt
       </Link></div>
